15dd418fdd82e8154cee11bce7edd81a51fa2c3b
icopavlov <44852517+icopavlov@users.noreply.github.com>
2019-08-05 10:12:11 +0300

    Add files via upload

---
+++ js/handlebars-template-loading.js
@@ -0,0 +1,27 @@
+/*-----------------------------------------------------------------*/
+/*	Handlebars
+/*-----------------------------------------------------------------*/
+
+/**
+ * Loading section content through Handlebars
+ */
+$( document ).ready( function() {
+	
+	loadTemplate( "body > header", "#header-template", headerContent );
+	loadTemplate( "#slider", "#slider-template", sliderContent );
+	loadTemplate( "#about", "#about-template", aboutContent );
+	loadTemplate( "#services", "#services-template", servicesContent );
+	loadTemplate( "#showcase", "#showcase-template", showcaseContent );
+	loadTemplate( "#live-templates", "#live-templates-template", liveTemplatesContent );
+	loadTemplate( "#contact-info", "#contact-info-template", contactInfoContent );
+	loadTemplate( "#send-email", "#send-email-template", sendEmailContent );
+	loadTemplate( "footer", "#footer-template", footerContent );
+	
+	function loadTemplate( renderedContentHolderSelector, templateHolderSelector, templateContext ) {
+		let template = $( templateHolderSelector ).html(),
+			templateCompiled = Handlebars.compile( template ),
+			contentRendered = templateCompiled( templateContext );
+		$( renderedContentHolderSelector ).append( contentRendered );
+	}
+
+} );
